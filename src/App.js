f854df44661c336f7a6a4d388b35d440acc9f144
nyxiaaa <126647495+nyxiaaa@users.noreply.github.com>
2024-11-23 10:29:56 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,21 +8,23 @@ import Reservation from "./pages/Reservation";
 import Dashboard from "./pages/Dashboard";
 import Locations from "./pages/Locations";
 import Reviews from "./pages/Reviews";
-
+import PageLoaderWrapper from "./Components/PageLoaderWrapper"; // Import the component
 
 function App() {
   return (
     <Router>
-      <NavigationBar /> {/* Include the navigation bar */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/reserve" element={<Reservation />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/locations" element={<Locations />} />
-        <Route path="/reviews" element={<Reviews />} />
-      </Routes>
+      <PageLoaderWrapper>
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reserve" element={<Reservation />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/locations" element={<Locations />} />
+          <Route path="/reviews" element={<Reviews />} />
+        </Routes>
+      </PageLoaderWrapper>
     </Router>
   );
 }
